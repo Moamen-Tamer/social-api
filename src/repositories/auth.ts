62e430814131e9a21c9dc03bf11a608751a2ba8d
Mo'men Tamer <moamenmosa288@gmail.com>
2026-08-13 14:03:58 +0300

    feat(auth): switch from postgres to knex in auth repo

--- src/repositories/auth.ts
+++ src/repositories/auth.ts
@@ -1,46 +1,43 @@
+import { db } from "../connections/knex.js";
 import { pool } from "../connections/postgres.js";
 import type { User } from "../types/blueprints.js";
 
 export const existsByEmail = async (email: string): Promise<boolean> => {
-    const { rowCount } = await pool.query(
-        `SELECT 1
-         FROM users
-         WHERE email = $1
-         LIMIT 1`,
-        [email]
-    );
+    const user = await db("user")
+        .select("id")
+        .where("email", email)
+        .first();
 
-    return rowCount !== 0;
+    return user !== undefined;
 };
 
 export const existsByUsername = async (username: string): Promise<boolean> => {
-    const { rowCount } = await pool.query(
-        `SELECT 1
-         FROM users
-         WHERE username = $1
-         LIMIT 1`,
-        [username]
-    );
+    const user = await db("user")
+        .select("id")
+        .where("username", username)
+        .first();
 
-    return rowCount !== 0;
+    return user !== undefined;
 };
 
 export const createUser = async (username: string, email: string, password: string): Promise<void> => {
-    await pool.query(
-        `INSERT INTO users (username, email, password_hash)
-         VALUES ($1, $2, $3)`,
-        [username, email, password]
-    );
+    await db("users").insert({
+        username,
+        email,
+        password_hash: password
+    });
 };
 
 export const getUser = async (email: string): Promise<User | null> => {
-    const { rows } = await pool.query<User>(
-        `SELECT id, username, email, password_hash 
-         FROM users
-         WHERE email = $1
-         LIMIT 1`,
-        [email]
-    );
+    const user = await db("users")
+        .select(
+            "id",
+            "username",
+            "email",
+            "password_hash"
+        )
+        .where("email", email)
+        .first();
 
-    return rows[0] ?? null;
+    return user ?? null;
 };
